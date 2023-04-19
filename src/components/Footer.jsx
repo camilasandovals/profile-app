78acce186bd7d4d67e47cedf212af80ce6f6a75f
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-19 15:25:27 -0400

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
+    const gitHubUrl = "https://github.com/milasandovals/profile-app"
     return (
         <footer>
-            <p>&copy; - Camila Sandoval</p>
+            <Container>
+                <Row>
+                    <Col className="text-center">
+                        <p>&copy; {currentYear} - Camila Sandoval
+                        <br />
+                            <a href={gitHubUrl}
+                            target="_blank"
+                            rel="noreferrer">Code in GitHub</a>
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     )
 }
