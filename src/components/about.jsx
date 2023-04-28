8c26e1602a8bc5bf8c54d7b16536851245b7b623
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-27 20:37:49 -0400

    updated hooks seen in class

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,18 +1,38 @@
+import { useState } from "react"
 import { Col, Container, Row, Button, Modal } from "react-bootstrap";
 
+const randomElement = (array) => {
+    const index = Math.floor(Math.random() * array.length)
+    return array[index]
+}
+
+const imageList = [
+  `https://picsum.photos/300/300/?random=2`,
+  `https://picsum.photos/300/300/?random=4`,
+  `https://picsum.photos/300/300/?random=6`,
+  `https://picsum.photos/300/300/?random=8`,
+]
+
+const colors = ['red', 'blue', 'black', 'salmon']
+
 export default function About() {
+    const [imgUrl, setImgUrl] = useState(randomElement(imageList))
+    const [nameColor, setNameColor] = useState(randomElement(colors))
     return (
         <main>
             <Container>
             {/* <Container className="bg-danger"> */}
                 <Row className="text-center">
                     <Col>
-                    <img src="https://picsum.photos/seed/picsum/300/300" 
+                    <img src={imgUrl} onClick = {() => 
+                        setImgUrl(randomElement(imageList))
+                    } 
                     className="rounded-circle"
                     alt="Camila Sandoval" />
+                    <p>click image </p>
                     </Col>
                     <Col sm={12} md={6}>
-                    <h1 className="mt-3"> Camila Sandoval </h1>
+                    <h1 className="mt-3" onClick={() => {setNameColor(randomElement(colors))}} style={{ color: nameColor }}> Camila Sandoval </h1>
                     <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur laboriosam, dolorum, delectus accusantium aspernatur id quis sapiente obcaecati odio neque alias repellendus sequi similique magnam veniam! Magni voluptas id nulla.</p>
                     <Button>My Video</Button>
                     
